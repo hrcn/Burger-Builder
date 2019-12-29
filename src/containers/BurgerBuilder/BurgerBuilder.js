065624c8c9651b5af71177367bbbb79c8c9801c8
Rui Han <hrcn1128@gmail.com>
2019-12-29 15:02:56 -0500

    output ingredients dynamically

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+
+import Auxi from '../../hoc/Auxi';
+import Burger from '../../components/Burger/Burger';
+
+class BurgerBuilder extends Component {
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 1,
+            cheese: 2,
+            meat: 2
+        }
+    }
+
+    render() {
+        return (
+            <Auxi>
+                <Burger ingredients={this.state.ingredients} />
+                <div>Build Controls</div>
+            </Auxi>
+        );
+    }
+}
+
+export default BurgerBuilder;
